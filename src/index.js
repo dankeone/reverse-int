857766d220eec4123ec91f328e1807d211e41c30
dankeone <sg10110000@gmail.com>
2020-03-20 22:25:31 +0300

    fix: add new index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  	n +="";
+	let str = n.split('');
+	let rever = str.reverse('');
+	rever = rever.join('');
+	let res = parseInt(rever);
+	return(res);
+
 }
